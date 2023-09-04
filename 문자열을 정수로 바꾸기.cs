7940ffdfe7ade40ce83fc0d284c18ff7d0c88c97
KHS <kij4300@naver.com>
2023-09-04 10:57:38 +0900

    Create 문자열을 정수로 바꾸기.cs

---
+++ 문자열을 정수로 바꾸기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(string s) {
+        
+        int number = Int32.Parse(s);
+
+        return number;
+        
+        //int answer = 0;
+        //return answer;
+    }
+}
