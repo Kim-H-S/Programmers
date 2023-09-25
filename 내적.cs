35937d53737c7a7bb55f9c696e24baebcf9eac70
KHS <kij4300@naver.com>
2023-09-25 10:49:14 +0900

    Create 내적.cs

---
+++ 내적.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int solution(int[] a, int[] b) {
+        //int answer = 1234567890;
+        int answer = 0;
+        int sum = 0;
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            sum += a[i] * b[i];
+        }
+
+        answer = sum;
+        
+        return answer;
+    }
+}
