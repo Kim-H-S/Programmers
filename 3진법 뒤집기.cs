c538dd7b0e4b08e731abfba6ad7fd856a0796086
KHS <kij4300@naver.com>
2023-10-11 09:45:28 +0900

    Create 3진법 뒤집기.cs

---
+++ 3진법 뒤집기.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        List<int> list = new List<int>();
+
+        while (true) 
+        {
+            if (n == 0)
+            {
+                break;
+            }
+
+            int num = n % 3;
+
+            list.Add(num);
+
+            n = n / 3;
+        }
+
+        list.Reverse();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            answer += (int)(list[i] * Math.Pow(3, i));
+        }
+        
+        return answer;
+    }
+}
